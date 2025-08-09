eac38c75a83e7000fc8f12910e42529853ff563b
Kasra_iran13 <amiralisharifian1388@gmail.com>
2025-08-09 22:15:09 +0330

    Add files

---
+++ README.md
@@ -0,0 +1,34 @@
+## path_finder
+
+**path_finder** is a small Python tool in one file. It helps you import code from folders outside your current folder.
+
+### What does it do?
+Sometimes in a project, you want to use code from other folders that are not inside your current folder. This tool:
+
+- Checks if you have a `.gitignore` file.
+- Reads the `.gitignore` file and finds folders you want to ignore.
+- Adds all folders except the ignored ones to Python’s `sys.path`.
+- So you can import code from outside folders easily.
+
+### How to use?
+Just import it at the start of your script:
+
+```python
+import path_finder
+```
+That’s it! It will fix your import paths automatically.
+
+### How to get it?
+You can clone the project from GitHub:
+
+```bash
+git clone https://github.com/kasrairan13/path-finder.git
+```
+
+### Why use it?
+- It is easy to use, only one file.
+- It reads .gitignore and ignores folders you don’t want.
+- It helps you import code from other folders without problems.
+
+### License
+This project is free and open source under the MIT License.
---
+++ path_finder.py
@@ -0,0 +1,69 @@
+from pathlib import Path as path_lib
+from sys import path as sys_path
+from typing import Generator
+
+
+__all__ = []
+
+
+class _GitIgnore:
+    def __init__(self) -> None:
+        self.file_name = ".gitignore"
+        self.excluded_dirs = list()
+
+        if self._exists():
+            self.main_file = path_lib(__file__).resolve()
+            self.main_folder = self.main_file.parent
+
+            self.text = self._open_file()
+            self._remove_base_folder()
+
+    def _exists(self) -> bool:
+        return path_lib(self.file_name).exists()
+    
+    def _open_file(self) -> list[str]:
+        text = path_lib(self.file_name).read_text()
+        return text.splitlines()
+    
+    def _get_lines(self) -> Generator:
+        for line in self.text:
+            if "/" in line and not line.startswith("#"):
+                yield line
+
+    def _get_dirs(self) -> Generator:
+        for directory in self._get_lines():
+            directory = directory.split(".")
+            if len(directory) >= 2 and "/" not in directory[-1]:
+                continue
+            yield directory
+
+    def _complete_paths(self) -> Generator:
+        for directory in self._get_dirs():
+            path = self.main_folder / directory[0]
+            yield str(path)
+
+    def _remove_base_folder(self) -> None:
+        for directory in self._complete_paths():
+            if directory != str(self.main_folder):
+                self.excluded_dirs.append(directory)
+
+
+class _Path(_GitIgnore):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._add()
+
+    def _scan_dirs(self) -> Generator:
+        for directory in self.main_folder.rglob('*'):
+            directory = str(directory)
+            if any(directory.startswith(excluded) for excluded in self.excluded_dirs):
+                continue
+            yield directory
+
+    def _add(self) -> None:
+        for paths in self._scan_dirs():
+            sys_path.append(paths)
+
+
+instance = _Path()
